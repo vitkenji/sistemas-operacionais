33609e47bf9ceadebdaac5315f2ea9adfac77fbf
Ariel Carvalho <arielwcarvalho@gmail.com>
2026-05-02 13:41:35 -0300

    Apenas adicionado testes na main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <GLFW/glfw3.h>
+#include "gerenciadores/GerenciadorTarefa.hpp"
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "tarefa/tarefa.hpp"
 #include <iostream>
+#include <vector>
 
 int main() {
     // 1. Inicializa o GLFW e cria a janela
@@ -32,8 +35,36 @@ int main() {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 130");
 
+    GerenciadorTarefa* gerenciadorTarefa = GerenciadorTarefa::getInstance("priop");
+
+    std::vector<int> eventosTarefa1;
+    std::vector<int> eventosTarefa2;
+    std::vector<int> eventosTarefa3;
+
+    Tarefa tarefa1(1, 0, 5, 1, eventosTarefa1);
+    Tarefa tarefa2(2, 1, 3, 2, eventosTarefa2);
+    Tarefa tarefa3(3, 2, 4, 3, eventosTarefa3);
+
+    tarefa1.registrarEstadoNoTempo(0, EstadoTarefa::Nova);
+    tarefa2.registrarEstadoNoTempo(0, EstadoTarefa::Nova);
+    tarefa3.registrarEstadoNoTempo(0, EstadoTarefa::Nova);
+
+    gerenciadorTarefa->adicionarTarefa(tarefa1);
+    gerenciadorTarefa->adicionarTarefa(tarefa2);
+    gerenciadorTarefa->adicionarTarefa(tarefa3);
+
+    int contadorLoop = 1;
+    int tempoAtual = 1;
+
     // 4. Loop Principal
     while (!glfwWindowShouldClose(window)) {
+        if (contadorLoop % 100 == 0)
+        {
+            gerenciadorTarefa->avancaTempo(tempoAtual);
+            tempoAtual++;
+        }
+        contadorLoop++;
+
         // Processa os eventos da janela (teclado, mouse, etc)
         glfwPollEvents();
 
